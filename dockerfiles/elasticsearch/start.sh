f4d7fe3525eb13043433ee8a7e3ab731a9384186
Qiu Yu <unicell@gmail.com>
2016-04-27 00:20:53 +0000

    ensure ownership of elasticsearch data volume

--- dockerfiles/elasticsearch/start.sh
+++ dockerfiles/elasticsearch/start.sh
@@ -2,6 +2,7 @@
 
 # workaround for https://github.com/docker/docker/issues/7198
 chown -R elasticsearch:elasticsearch /repo
+chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 
 echo "Starting ElasticSearch"
 gosu elasticsearch elasticsearch
